99bd377f9200f1c3adc5ae5c0ad75c5f0432db1c
hcaroli381 <hcaroli381@g.educaand.es>
2026-02-25 09:07:16 +0100

    inicio baraja

---
+++ src/blackjack/dominio/Baraja.java
@@ -0,0 +1,14 @@
+package blackjack.dominio;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Baraja {
+	public List<Carta> baraja = new ArrayList();
+
+	public void crearBaraja() {
+		for (int i = 0; i < 52; i++) {
+
+		}
+	}
+}
--- src/blackjack/dominio/Carta.java
+++ src/blackjack/dominio/Carta.java
@@ -5,8 +5,27 @@ public class Carta {
 	private TipoCarta tipo;
 
 	public Carta(Palo palo, TipoCarta tipo) {
-		this.palo = palo;
+		this.setPalo(palo);
+		this.setTipo(tipo);
+	}
+
+	public TipoCarta getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(TipoCarta tipo) {
 		this.tipo = tipo;
 	}
 
+	public Palo getPalo() {
+		return palo;
+	}
+
+	public void setPalo(Palo palo) {
+		this.palo = palo;
+	}
+
+	public String toString() {
+		return String.format("%s de %s", tipo.toString(), palo.toString());
+	}
 }
