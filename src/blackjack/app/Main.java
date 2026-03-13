f43efeaad84cb47983bf333641722dfeb486041b
hcaroli381 <hcaroli381@g.educaand.es>
2026-03-13 14:24:10 +0100

    bucle bienvenida actualizado

--- src/blackjack/app/Main.java
+++ src/blackjack/app/Main.java
@@ -17,86 +17,88 @@ public class Main {
 					"Bienvenido al Casino Saladillo, pulse j para jugar o s para salir :");
 			if (opcion) {
 				elegirModo = consola.readBooleanUsingChar('p', 'f', "p para jugar PVP f para jugar vs IA : ");
-			}
-			if (elegirModo) {
-				// partida con 2 jugadores reales
 
-				nombre1 = consola.leerTextoNoVacio("Introduce el nombre del primer jugador : ");
-				nombre2 = consola.leerTextoNoVacio("Introduce el nombre del segundo jugador : ");
-				partida = new Partida(nombre1, nombre2);
-				partida.iniciarPartida();
-				consola.escribirLinea(partida.toString());
-				while (!partida.finPartida(pedir1, pedir2)) {
-					if (pedir1 == true && partida.getJugador1().puntuacion() != 21) {
-						pedir1 = consola.readBooleanUsingChar('p', 'f',
-								nombre1 + " escribe p para pedir o f para plantarte : ");
-					} else {
-						pedir1 = false;
-					}
-					if (pedir2 == true && partida.getJugador2().puntuacion() != 21) {
-						pedir2 = consola.readBooleanUsingChar('p', 'f',
-								nombre2 + " escribe p para pedir o f para plantarte : ");
-					} else {
-						pedir2 = false;
-					}
+				if (elegirModo) {
+					// partida con 2 jugadores reales
 
-					partida.pedirCarta(pedir1, pedir2);
+					nombre1 = consola.leerTextoNoVacio("Introduce el nombre del primer jugador : ");
+					nombre2 = consola.leerTextoNoVacio("Introduce el nombre del segundo jugador : ");
+					partida = new Partida(nombre1, nombre2);
+					partida.iniciarPartida();
 					consola.escribirLinea(partida.toString());
-
-				}
-			} else {
-				// partida contra el crupier
-				pedir2 = false;
-				nombre1 = consola.leerTextoNoVacio("Introduce el nombre del primer jugador : ");
-				nombre2 = "Crupier";
-				partida = new Partida(nombre1, false);
-				partida.iniciarPartida();
-
-				consola.escribirLinea(partida.toString());
-				while (!partida.finPartida(pedir1, pedir2)) {
-					if (pedir1 == true && partida.getJugador1().puntuacion() != 21) {
-						pedir1 = consola.readBooleanUsingChar('p', 'f',
-								nombre1 + " escribe p para pedir o f para plantarte : ");
-
-					} else {
-						pedir1 = false;
-					}
-					if (!pedir1) {
-						if ((partida.getJugador1().puntuacion() > partida.getJugador2().puntuacion())) {
-							pedir2 = true;
-							((Crupier) partida.getJugador2()).setTurno(true);
+					while (!partida.finPartida(pedir1, pedir2)) {
+						if (pedir1 == true && partida.getJugador1().puntuacion() != 21) {
+							pedir1 = consola.readBooleanUsingChar('p', 'f',
+									nombre1 + " escribe p para pedir o f para plantarte : ");
+						} else {
+							pedir1 = false;
 						}
-						if ((partida.getJugador1().puntuacion() < partida.getJugador2().puntuacion())
-								|| (partida.getJugador1().puntuacion() == partida.getJugador2().puntuacion())) {
+						if (pedir2 == true && partida.getJugador2().puntuacion() != 21) {
+							pedir2 = consola.readBooleanUsingChar('p', 'f',
+									nombre2 + " escribe p para pedir o f para plantarte : ");
+						} else {
 							pedir2 = false;
 						}
-						if (((partida.getJugador1().puntuacion() < partida.getJugador2().puntuacion())
-								&& pedir1 == false)) {
-							((Crupier) partida.getJugador2()).setTurno(true);
-						}
+
+						partida.pedirCarta(pedir1, pedir2);
+						consola.escribirLinea(partida.toString());
+
 					}
-					partida.pedirCarta(pedir1, pedir2);
+				} else {
+					// partida contra el crupier
+					pedir2 = false;
+					nombre1 = consola.leerTextoNoVacio("Introduce el nombre del primer jugador : ");
+					nombre2 = "Crupier";
+					partida = new Partida(nombre1, false);
+					partida.iniciarPartida();
+
 					consola.escribirLinea(partida.toString());
-					// retraso para poder ver que hace el crupier
-					Thread.sleep(2000);
+					while (!partida.finPartida(pedir1, pedir2)) {
+						if (pedir1 == true && partida.getJugador1().puntuacion() != 21) {
+							pedir1 = consola.readBooleanUsingChar('p', 'f',
+									nombre1 + " escribe p para pedir o f para plantarte : ");
 
+						} else {
+							pedir1 = false;
+						}
+						if (!pedir1) {
+							if ((partida.getJugador1().puntuacion() > partida.getJugador2().puntuacion())) {
+								pedir2 = true;
+								((Crupier) partida.getJugador2()).setTurno(true);
+							}
+							if ((partida.getJugador1().puntuacion() < partida.getJugador2().puntuacion())
+									|| (partida.getJugador1().puntuacion() == partida.getJugador2().puntuacion())) {
+								pedir2 = false;
+							}
+							if (((partida.getJugador1().puntuacion() < partida.getJugador2().puntuacion())
+									&& pedir1 == false)) {
+								((Crupier) partida.getJugador2()).setTurno(true);
+							}
+						}
+						partida.pedirCarta(pedir1, pedir2);
+						consola.escribirLinea(partida.toString());
+						// retraso para poder ver que hace el crupier
+						Thread.sleep(2000);
+
+					}
 				}
-			}
 
-			if ((partida.getJugador1().puntuacion() > partida.getJugador2().puntuacion()
-					&& partida.getJugador1().puntuacion() <= 21)
-					|| (partida.getJugador1().puntuacion() <= 21 && partida.getJugador2().puntuacion() > 21)) {
-				consola.escribirLinea(nombre1 + " gana!!");
-			} else if ((partida.getJugador2().puntuacion() > partida.getJugador1().puntuacion()
-					&& partida.getJugador2().puntuacion() <= 21)
-					|| (partida.getJugador2().puntuacion() <= 21 && partida.getJugador1().puntuacion() > 21)) {
-				consola.escribirLinea(nombre2 + " gana!!");
-			} else {
-				consola.escribirLinea("Empate!!");
-			}
-			System.out.printf("La siguiente carta era %s", partida.getBaraja().extraerCarta().toString());
-			if (!elegirModo) {
-				pedir1 = true;
+				if ((partida.getJugador1().puntuacion() > partida.getJugador2().puntuacion()
+						&& partida.getJugador1().puntuacion() <= 21)
+						|| (partida.getJugador1().puntuacion() <= 21 && partida.getJugador2().puntuacion() > 21)) {
+					consola.escribirLinea(nombre1 + " gana!!");
+				} else if ((partida.getJugador2().puntuacion() > partida.getJugador1().puntuacion()
+						&& partida.getJugador2().puntuacion() <= 21)
+						|| (partida.getJugador2().puntuacion() <= 21 && partida.getJugador1().puntuacion() > 21)) {
+					consola.escribirLinea(nombre2 + " gana!!");
+				} else {
+					consola.escribirLinea("Empate!!");
+				}
+				consola.escribirLinea(
+						String.format("La siguiente carta era %s", partida.getBaraja().extraerCarta().toString()));
+				if (!elegirModo) {
+					pedir1 = true;
+				}
 			}
 		} while (opcion);
 
